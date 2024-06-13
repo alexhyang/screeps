370fa92e2fd6d6dc49c12d82e47045cef2e50e48
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-13 04:18:24 -0700

    refactor builder and upgrader

--- default/role.builder.js
+++ default/role.builder.js
@@ -3,6 +3,15 @@ const resources = require("resources");
 var roleBuilder = {
   /** @param {Creep} creep **/
   run: function (creep) {
+    this.updateBuildingStatus(creep);
+    if (creep.memory.building) {
+      this.buildConstructionSite(creep);
+    } else {
+      this.obtainEnergy(creep);
+    }
+  },
+  /** @param {Creep} creep **/
+  updateBuildingStatus: function (creep) {
     if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
       creep.memory.building = false;
       creep.say("🔄 harvest");
@@ -12,38 +21,46 @@ var roleBuilder = {
       creep.memory.building = true;
       creep.say("🚧 build");
     }
-
-    if (creep.memory.building) {
-      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-      if (targets.length) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0], {
-            visualizePathStyle: { stroke: "#ffffff" },
-          });
-        }
+  },
+  /** @param {Creep} creep **/
+  buildConstructionSite: function (creep) {
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    if (targets.length) {
+      if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(targets[0], {
+          visualizePathStyle: { stroke: "#ffffff" },
+        });
       }
+    }
+  },
+  /** @param {Creep} creep **/
+  obtainEnergy: function (creep) {
+    if (resources.withdrawOk()) {
+      this.obtainEnergyFromSpawn(creep);
     } else {
-      if (resources.withdrawOk()) {
-        var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
-        if (
-          resources.withdrawOk() &&
-          creep.pos.getRangeTo(spawn) == 1 &&
-          creep.withdraw(spawn, RESOURCE_ENERGY) == OK
-        ) {
-          creep.memory.building = true;
-        } else {
-          creep.moveTo(spawn, {
-            visualizePathStyle: { stroke: "#ffaa00" },
-          });
-        }
-      } else {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources[0], {
-            visualizePathStyle: { stroke: "#ffaa00" },
-          });
-        }
-      }
+      this.obtainEnergyFromSource(creep);
+    }
+  },
+  obtainEnergyFromSpawn: function (creep) {
+    var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
+    if (
+      resources.withdrawOk() &&
+      creep.pos.getRangeTo(spawn) == 1 &&
+      creep.withdraw(spawn, RESOURCE_ENERGY) == OK
+    ) {
+      creep.memory.building = true;
+    } else {
+      creep.moveTo(spawn, {
+        visualizePathStyle: { stroke: "#ffaa00" },
+      });
+    }
+  },
+  obtainEnergyFromSource: function (creep) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[0], {
+        visualizePathStyle: { stroke: "#ffaa00" },
+      });
     }
   },
 };
--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -1,6 +1,15 @@
 var roleUpgrader = {
   /** @param {Creep} creep **/
   run: function (creep) {
+    this.updateUpgradingStatus(creep);
+    if (creep.memory.upgrading) {
+      this.upgradeController(creep);
+    } else {
+      this.obtainEnergy(creep);
+    }
+  },
+  /** @param {Creep} creep **/
+  updateUpgradingStatus: function (creep) {
     if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
       creep.memory.upgrading = false;
       creep.say("🔄 harvest");
@@ -10,18 +19,20 @@ var roleUpgrader = {
       creep.memory.upgrading = true;
       creep.say("⚡ upgrade");
     }
-
-    if (creep.memory.upgrading) {
-      if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.room.controller, {
-          visualizePathStyle: { stroke: "#ffffff" },
-        });
-      }
-    } else {
-      var sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
-      }
+  },
+  /** @param {Creep} creep **/
+  upgradeController: function (creep) {
+    if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(creep.room.controller, {
+        visualizePathStyle: { stroke: "#ffffff" },
+      });
+    }
+  },
+  /** @param {Creep} creep **/
+  obtainEnergy: function (creep) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
     }
   },
 };
