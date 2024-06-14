1ad68f5d238ee54b322b1e21cd6485d66e793d75
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-13 22:19:16 -0700

    improve logger title

--- default/logger.js
+++ default/logger.js
@@ -1,17 +1,33 @@
 var squadLogger = require("./logger.squad");
-const { ENERGY_AVAILABLE, ENERGY_CAPACITY_AVAILABLE } = require("./dashboard");
+const {
+  ROOM,
+  ENERGY_AVAILABLE,
+  ENERGY_CAPACITY_AVAILABLE,
+} = require("./dashboard");
 
 var logger = {
   log: function () {
+    this.printLogTitle();
+    squadLogger.log();
+    console.log("\n");
+  },
+  printLogTitle: function () {
+    let energyMeta = `Energy: ${ENERGY_AVAILABLE}/${ENERGY_CAPACITY_AVAILABLE}`;
+    let controller = ROOM.controller;
+    let current = controller.progress;
+    let total = controller.progressTotal;
+    let controllerMeta = `Controller (lvl. ${controller.level} ${Math.round(
+      (current / total) * 100
+    )}%: ${current}/${total})`;
     console.log(
       "--------------  " +
         Game.time +
         " // " +
-        `Energy: ${ENERGY_AVAILABLE}/${ENERGY_CAPACITY_AVAILABLE}` +
+        energyMeta +
+        " | " +
+        controllerMeta +
         " -------------"
     );
-    squadLogger.log();
-    console.log("\n");
   },
 };
 
