994396be635491a8c9ab5ebe647a183c4a7bbdfb
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-17 05:19:33 -0700

    shorten creep names

--- src/dashboard.models.js
+++ src/dashboard.models.js
@@ -16,161 +16,161 @@
 let MODELS = {
   // ======== HARVESTERS ========
   HARVESTER_200: {
-    name: "H200",
+    name: "H2",
     role: "harvester",
     body: [WORK, CARRY, MOVE],
   },
   HARVESTER_250: {
-    name: "H250",
+    name: "H25",
     role: "harvester",
     body: [WORK, CARRY, CARRY, MOVE],
   },
   HARVESTER_300: {
-    name: "H300",
+    name: "H3",
     role: "harvester",
     body: [WORK, WORK, CARRY, MOVE],
   },
   HARVESTER_350_FAST: {
-    name: "H350F",
+    name: "H35F",
     role: "harvester",
     body: [WORK, WORK, CARRY, MOVE, MOVE],
   },
   HARVESTER_350_LARGE: {
-    name: "H350L",
+    name: "H35L",
     role: "harvester",
     body: [WORK, WORK, CARRY, CARRY, MOVE],
   },
   HARVESTER_400_FAST: {
-    name: "H400F",
+    name: "H4F",
     role: "harvester",
     body: [WORK, WORK, CARRY, MOVE, MOVE, MOVE],
   },
   HARVESTER_400_CARRY: {
-    name: "H400C",
+    name: "H4C",
     role: "harvester",
     body: [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
   },
   HARVESTER_450: {
-    name: "H450",
+    name: "H45",
     role: "harvester",
     body: [WORK, WORK, WORK, CARRY, MOVE, MOVE],
   },
   HARVESTER_500: {
-    name: "H500",
+    name: "H5",
     role: "harvester",
     body: [WORK, WORK, WORK, WORK, CARRY, MOVE],
   },
   HARVESTER_550: {
-    name: "H550",
+    name: "H55",
     role: "harvester",
     body: [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
   },
   // ======== MINERS ========
   MINER_200: {
-    name: "M200",
+    name: "M2",
     role: "miner",
     body: [WORK, CARRY, MOVE],
   },
   MINER_300: {
-    name: "M300",
+    name: "M3",
     role: "miner",
     body: Array(2).fill(WORK).concat([CARRY, MOVE]),
   },
   MINER_400: {
-    name: "M400",
+    name: "M4",
     role: "miner",
     body: Array(3).fill(WORK).concat([CARRY, MOVE]),
   },
   MINER_500: {
-    name: "M500",
+    name: "M5",
     role: "miner",
     body: Array(4).fill(WORK).concat([CARRY, MOVE]),
   },
   MINER_600: {
-    name: "M600",
+    name: "M6",
     role: "miner",
     body: Array(5).fill(WORK).concat([CARRY, MOVE]),
   },
   MINER_700: {
-    name: "M700",
+    name: "M7",
     role: "miner",
     body: Array(6).fill(WORK).concat([CARRY, MOVE]),
   },
   MINER_1150: {
-    name: "M1150",
+    name: "M115",
     role: "miner",
     body: Array(10).fill(WORK).concat([CARRY, MOVE, MOVE]),
   },
   // ======== BUILDERS ========
   BUILDER_200: {
-    name: "B200",
+    name: "B2",
     role: "builder",
     body: [WORK, CARRY, MOVE],
   },
   BUILDER_250_FAST: {
-    name: "B250F",
+    name: "B25F",
     role: "builder",
     body: [WORK, CARRY, MOVE, MOVE],
   },
   BUILDER_250_LARGE: {
-    name: "B250L",
+    name: "B25L",
     role: "builder",
     body: [WORK, CARRY, CARRY, MOVE],
   },
   BUILDER_350_FAST: {
-    name: "B350F",
+    name: "B35F",
     role: "builder",
     body: [WORK, WORK, CARRY, MOVE, MOVE],
   },
   BUILDER_350_LARGE: {
-    name: "B350L",
+    name: "B35L",
     role: "builder",
     body: [WORK, WORK, CARRY, CARRY, MOVE],
   },
   BUILDER_400_FAST: {
-    name: "B400F",
+    name: "B4F",
     role: "builder",
     body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
   },
   BUILDER_400_LARGE: {
-    name: "B400L",
+    name: "B4L",
     role: "builder",
     body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
   },
   // ======== UPGRADERS ========
   UPGRADER_200: {
-    name: "U200",
+    name: "U2",
     role: "upgrader",
     body: [WORK, CARRY, MOVE],
   },
   UPGRADER_400: {
-    name: "U400",
+    name: "U4",
     role: "upgrader",
     body: [WORK, WORK, WORK, CARRY, MOVE],
   },
   UPGRADER_500: {
-    name: "U500",
+    name: "U5",
     role: "upgrader",
     body: [WORK, WORK, WORK, WORK, CARRY, MOVE],
   },
   UPGRADER_600: {
-    name: "U600",
+    name: "U6",
     role: "upgrader",
     body: [WORK, WORK, WORK, WORK, WORK, CARRY, MOVE],
   },
   // ======== REPAIRERS ========
   REPAIRER_250: {
-    name: "R250",
+    name: "R25",
     role: "repairer",
     body: [WORK, CARRY, MOVE, MOVE],
   },
   REPAIRER_400: {
-    name: "R400",
+    name: "R4",
     role: "repairer",
     body: [WORK, WORK, CARRY, MOVE, MOVE, MOVE],
   },
   REPAIRER_400: {
-    name: "R400",
+    name: "R4",
     role: "repairer",
     body: [WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE, MOVE],
   },
