07615661c6deeaeb23592ca70b9af18c5a83a31e
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-17 04:45:33 -0700

    add tower to harvest transfer list

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -28,8 +28,8 @@ var roleHarvester = {
         if (transferToNonContainer) {
           return (
             (structure.structureType == STRUCTURE_EXTENSION ||
-              structure.structureType == STRUCTURE_SPAWN) &&
-            //structure.structureType == STRUCTURE_TOWER) &&
+              structure.structureType == STRUCTURE_SPAWN ||
+              structure.structureType == STRUCTURE_TOWER) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         } else {
