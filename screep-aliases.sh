07c123a89dca27399d94dff21259de194ecc5576
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-17 03:28:50 -0700

    add message to shell task

--- screep-aliases.sh
+++ screep-aliases.sh
@@ -1,3 +1,3 @@
-alias dashboard="cp src/dashboard.js dist/dashboard.js"
+alias dashboard="cp src/dashboard.js dist/dashboard.js; echo 'synced dashboard'"
 alias build="rm build/*; cp src/* build/"
-alias dist="cp src/* dist/"
+alias dist="cp src/* dist/; echo 'copied to dist'"
