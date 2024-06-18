15cc145a962f21a677af47365952210d45e65aea
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-18 04:42:17 -0700

    change picker dest to Storage

--- src/role.picker.js
+++ src/role.picker.js
@@ -1,21 +1,35 @@
-const { pickupDroppedResources } = require("./squad.resourceManager");
+const {
+  pickupDroppedResources,
+  assignCreepToObtainEnergyFromRuin,
+} = require("./squad.resourceManager");
 
 var rolePicker = {
   /** @param {Creep} creep **/
   run: function (creep) {
     if (creep.store.getFreeCapacity() > 0) {
-      pickupDroppedResources(creep);
+      pickupDroppedResources(creep) || assignCreepToObtainEnergyFromRuin(creep);
     } else {
       this.transferEnergy(creep);
     }
   },
   transferEnergy: function (creep) {
-    let spawn = creep.room.find(FIND_MY_SPAWNS)[0];
-    if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(spawn, {
+    let targets = this.findTargets(creep);
+    if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(targets[0], {
         visualizePathStyle: { stroke: "#ffffff" },
       });
     }
+  },
+  findTargets: function (creep) {
+    var targets = creep.room.find(FIND_STRUCTURES, {
+      filter: (structure) => {
+        return (
+          structure.structureType == STRUCTURE_STORAGE &&
+          structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+        );
+      },
+    });
+    return targets;
   },
 };
 
