e6c03e4885fd920ec752947540cc49bcc49dcf53
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-18 04:39:37 -0700

    mute upgrader

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -18,12 +18,12 @@ var roleUpgrader = {
   updateUpgradingStatus: function (creep) {
     if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
       creep.memory.upgrading = false;
-      creep.say("🔄 harvest");
+      // creep.say("🔄 harvest");
     }
 
     if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
       creep.memory.upgrading = true;
-      creep.say("⚡ upgrade");
+      // creep.say("⚡ upgrade");
     }
   },
   /** @param {Creep} creep **/
