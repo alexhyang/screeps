ccf69a0b81518a1f7ae5ed9285e13230732fe707
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-17 19:51:56 -0700

    update tower id and repair param

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const { TOP_TOWER, BOTTOM_TOWER } = require("./dashboard");
 module.exports.loop = function () {
   memoryManager.cleanNonExistingCreeps();
   defenses.activateTower(TOP_TOWER, false);
-  defenses.activateTower(BOTTOM_TOWER, false);
+  defenses.activateTower(BOTTOM_TOWER, true);
   squad.recruitSquad();
   squad.assignJobs();
   logger.log();
