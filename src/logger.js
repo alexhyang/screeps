50205ceb3550491efff993a32c2cd04a9b34ea23
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-21 03:13:17 -0700

    refactor logger

--- src/logger.js
+++ src/logger.js
@@ -1,116 +1,92 @@
 var squadLogger = require("./logger.squad");
 const { ROOM_NUMBER, TOWER_REPAIR_MIN_HITS } = require("./dashboard");
-const { roundTo } = require("./logger.utils");
+const { roundTo, parseNumber } = require("./logger.utils");
 const {
   getHealthyDefenses,
   getUnhealthyDefenses,
-  getTowers,
+  getStructure,
+  getController,
 } = require("./util.structureFinder");
 
-var logger = {
+const printLogTitle = () => {
+  console.log(
+    "=== " +
+      `${Game.time % 1000000}` +
+      " // " +
+      getEnergyMeta() +
+      " | " +
+      getDefensesMeta() +
+      " | " +
+      getControllerMeta() +
+      " ==="
+  );
+};
+
+const getEnergyMeta = () => {
+  let containerMeta = getContainerMeta();
+  let storageMeta = getStorageMeta();
+  let energyAvailable = Game.rooms[ROOM_NUMBER].energyAvailable;
+  let energyCapacityAvailable = Game.rooms[ROOM_NUMBER].energyCapacityAvailable;
+  let energyMeta =
+    `Energy (${containerMeta}, ${storageMeta}): ` +
+    `${energyAvailable}/${energyCapacityAvailable}`;
+  return energyMeta;
+};
+
+const getDefensesMeta = () => {
+  let numHealthyWallsRamparts = getHealthyDefenses(
+    TOWER_REPAIR_MIN_HITS
+  ).length;
+  let numUnhealthyWallsRamparts = getUnhealthyDefenses(
+    TOWER_REPAIR_MIN_HITS
+  ).length;
+  let towerRepairProgress =
+    numHealthyWallsRamparts +
+    "/" +
+    `${numHealthyWallsRamparts + numUnhealthyWallsRamparts}`;
+  let towerAvailableEnergy = _.map(getStructure(STRUCTURE_TOWER), (t) =>
+    t.store.getUsedCapacity(RESOURCE_ENERGY)
+  );
+
+  let defenseMeta =
+    `Towers (${towerAvailableEnergy}) ` +
+    `WallsRamparts (${parseNumber(TOWER_REPAIR_MIN_HITS)}): ` +
+    towerRepairProgress;
+  return defenseMeta;
+};
+
+const getControllerMeta = () => {
+  let controller = getController();
+  let current = parseNumber(controller.progress);
+  let total = parseNumber(controller.progressTotal);
+  let percentage = roundTo(
+    Math.round((controller.progress / controller.progressTotal) * 100),
+    1
+  );
+  let controllerMeta =
+    `Controller (${controller.level}): ` +
+    `${percentage}%: ${current}/${total}`;
+  return controllerMeta;
+};
+
+const getContainerMeta = () => {
+  return getStructureMeta(STRUCTURE_CONTAINER);
+};
+const getStorageMeta = () => {
+  return getStructureMeta(STRUCTURE_STORAGE);
+};
+
+const getStructureMeta = (structureType) => {
+  let target = getStructure(structureType)[0];
+  let targetUsedCapacity = target.store.getUsedCapacity(RESOURCE_ENERGY);
+  let targetMeta = `${parseNumber(targetUsedCapacity)}`;
+  return targetMeta;
+};
+
+module.exports = {
   log: function () {
-    this.printLogTitle();
+    printLogTitle();
     squadLogger.log();
     console.log("\n");
   },
-  printLogTitle: function () {
-    console.log(
-      "=== " +
-        `${Game.time % 1000000}` +
-        " // " +
-        this.getEnergyMeta() +
-        " | " +
-        this.getDefensesMeta() +
-        " | " +
-        this.getControllerMeta() +
-        " ==="
-    );
-  },
-  getEnergyMeta: function () {
-    let containerMeta = this.getContainerMeta();
-    let storageMeta = this.getStorageMeta();
-    let energyAvailable = Game.rooms[ROOM_NUMBER].energyAvailable;
-    let energyCapacityAvailable =
-      Game.rooms[ROOM_NUMBER].energyCapacityAvailable;
-    let energyMeta =
-      `Energy (${containerMeta}, ${storageMeta}): ` +
-      `${energyAvailable}/${energyCapacityAvailable}`;
-    return energyMeta;
-  },
-  getContainerMeta: function () {
-    return this.getStructureMeta(STRUCTURE_CONTAINER);
-  },
-  getStorageMeta: function () {
-    return this.getStructureMeta(STRUCTURE_STORAGE);
-  },
-  getDefensesMeta: function () {
-    let numHealthyWallsRamparts = getHealthyDefenses(
-      TOWER_REPAIR_MIN_HITS
-    ).length;
-    let numUnhealthyWallsRamparts = getUnhealthyDefenses(
-      TOWER_REPAIR_MIN_HITS
-    ).length;
-    let towerRepairProgress =
-      numHealthyWallsRamparts +
-      "/" +
-      `${numHealthyWallsRamparts + numUnhealthyWallsRamparts}`;
-    let towerAvailableEnergy = _.map(getTowers(), (t) =>
-      t.store.getUsedCapacity(RESOURCE_ENERGY)
-    );
-
-    let defenseMeta =
-      `Towers (${towerAvailableEnergy}) ` +
-      `WallsRamparts (${this.parseNumber(TOWER_REPAIR_MIN_HITS)}): ` +
-      towerRepairProgress;
-    return defenseMeta;
-  },
-  getStructureMeta: function (structureType) {
-    let targets = Game.spawns["Spawn1"].room.find(FIND_STRUCTURES, {
-      filter: (structure) => structure.structureType == structureType,
-    });
-    let target = targets[0];
-    let targetUsedCapacity = target.store.getUsedCapacity(RESOURCE_ENERGY);
-    let targetMeta = `${this.parseNumber(targetUsedCapacity)}`;
-    return targetMeta;
-  },
-  getControllerMeta: function () {
-    let controller = Game.rooms[ROOM_NUMBER].controller;
-    let current = this.parseNumber(controller.progress);
-    let total = this.parseNumber(controller.progressTotal);
-    let percentage = roundTo(
-      Math.round((controller.progress / controller.progressTotal) * 100),
-      1
-    );
-    let controllerMeta =
-      `Controller (${controller.level}): ` +
-      `${percentage}%: ${current}/${total}`;
-    return controllerMeta;
-  },
-  /**
-   *
-   * @param {number} number
-   * @returns {string} progress in text form with units "K", "M"
-   */
-  parseNumber: function (number) {
-    if (number >= 1000000) {
-      return this.convertNumToMillions(number, 2) + "M";
-    } else if (number >= 1000) {
-      return this.convertNumToThousands(number, 2) + "K";
-    } else {
-      return number;
-    }
-  },
-  convertNumToThousands: function (num, numOfDecimalPlaces) {
-    return roundTo(num / 1000, numOfDecimalPlaces);
-  },
-  /**
-   * @param {number} num
-   * @param {number} numOfDecimalPlaces
-   * @returns {number} the given number in millions
-   */
-  convertNumToMillions: function (num, numOfDecimalPlaces) {
-    return roundTo(num / 1000000, numOfDecimalPlaces);
-  },
 };
-
-module.exports = logger;
--- src/logger.utils.js
+++ src/logger.utils.js
@@ -1,38 +1,57 @@
-var utils = {
-  /**
-   * @param {string} str
-   * @returns {string} capitalized string of the given string
-   */
-  capitalize: function (str) {
-    if (str.length == 0) return str;
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-  },
-  /**
-   * @param {number} num
-   * @param {number} numOfDecimalPlaces
-   * @returns {number} the given number in thousands
-   */
-  convertNumToThousands: function (num, numOfDecimalPlaces) {
-    return this.roundTo(num / 1000, numOfDecimalPlaces);
-  },
-  /**
-   * @param {number} num
-   * @param {number} numOfDecimalPlaces
-   * @returns {number} the given number in millions
-   */
-  convertNumToMillions: function (num, numOfDecimalPlaces) {
-    return this.roundTo(num / 1000000, numOfDecimalPlaces);
-  },
-  /**
-   * @param {number} num
-   * @param {number} numOfDecimalPlaces
-   * @returns {number} the given number rounded to given number of decimal
-   * places
-   */
-  roundTo: function (num, numOfDecimalPlaces) {
-    let multiplier = Math.pow(10, numOfDecimalPlaces);
-    return Math.round(num * multiplier) / multiplier;
-  },
+/**
+ * @param {string} str
+ * @returns {string} capitalized string of the given string
+ */
+const capitalize = (str) => {
+  if (str.length == 0) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+/**
+ * @param {number} num
+ * @param {number} numOfDecimalPlaces
+ * @returns {number} the given number in thousands
+ */
+const convertNumToThousands = (num, numOfDecimalPlaces) => {
+  return roundTo(num / 1000, numOfDecimalPlaces);
+};
+/**
+ * @param {number} num
+ * @param {number} numOfDecimalPlaces
+ * @returns {number} the given number in millions
+ */
+const convertNumToMillions = (num, numOfDecimalPlaces) => {
+  return roundTo(num / 1000000, numOfDecimalPlaces);
+};
+/**
+ * @param {number} num
+ * @param {number} numOfDecimalPlaces
+ * @returns {number} the given number rounded to given number of decimal
+ * places
+ */
+const roundTo = (num, numOfDecimalPlaces) => {
+  let multiplier = Math.pow(10, numOfDecimalPlaces);
+  return Math.round(num * multiplier) / multiplier;
 };
 
-module.exports = utils;
+/**
+ *
+ * @param {number} number
+ * @returns {string} progress in text form with units "K", "M"
+ */
+const parseNumber = (number) => {
+  if (number >= 1000000) {
+    return convertNumToMillions(number, 2) + "M";
+  } else if (number >= 1000) {
+    return convertNumToThousands(number, 2) + "K";
+  } else {
+    return number;
+  }
+};
+
+module.exports = {
+  capitalize,
+  convertNumToThousands,
+  convertNumToMillions,
+  roundTo,
+  parseNumber,
+};
