d2eb64df6bbee6bb9ddeaf4b450d9ccc224804a2
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-20 18:51:39 -0700

    add index for roles

---
+++ src/role.js
@@ -0,0 +1,15 @@
+const roleHarvester = require("./role.harvester");
+const roleMiner = require("./role.miner");
+const roleUpgrader = require("./role.upgrader");
+const roleRepairer = require("./role.repairer");
+const roleBuilder = require("./role.builder");
+const rolePicker = require("./role.picker");
+
+module.exports = {
+  roleHarvester,
+  roleMiner,
+  roleUpgrader,
+  roleRepairer,
+  roleBuilder,
+  rolePicker,
+};
