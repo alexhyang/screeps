d818cee8cac8c9319f0f7a293396ef592c7abcd8
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-21 21:20:35 -0700

    refactor

--- src/role.builder.js
+++ src/role.builder.js
@@ -11,6 +11,49 @@ const {
 } = require("./squad.resourceManager");
 const { BUILDER_SOURCE_INDEX, BUILD_PRIORITY } = require("./dashboard");
 
+/** @param {Creep} creep **/
+const updateBuildingStatus = (creep) => {
+  if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+    creep.memory.building = false;
+    creep.say("⛏");
+  }
+
+  if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+    creep.memory.building = true;
+    creep.say("🔨");
+  }
+};
+
+const buildConstructionSite = (creep, structureType = "none") => {
+  let filter;
+  if (structureType == "none") {
+    filter = (s) => true;
+  } else {
+    filter = (s) => s.structureType === structureType;
+  }
+  let target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {
+    filter: filter,
+  });
+  if (target) {
+    build(creep, target);
+  }
+};
+
+/** @param {Creep} creep **/
+const obtainEnergy = (creep) => {
+  if (creep.store.getFreeCapacity() > 0) {
+    pickupDroppedResources(creep) ||
+      assignCreepToObtainEnergyFromTombstone(creep) ||
+      assignCreepToObtainEnergyFromRuin(creep) ||
+      assignCreepToObtainEnergyFromStorage(creep) ||
+      (withdrawFromContainerOk() &&
+        assignCreepToObtainEnergyFromContainer(creep)) ||
+      (withdrawFromSpawnOk() && assignCreepToObtainEnergyFromSpawn(creep));
+  } else {
+    assignCreepToObtainEnergyFromSource(creep, BUILDER_SOURCE_INDEX);
+  }
+};
+
 const build = (creep, target) => {
   if (creep.build(target) == ERR_NOT_IN_RANGE) {
     creep.moveTo(target, { visualizePathStyle: { stroke: "#ffffff" } });
@@ -20,52 +63,11 @@ const build = (creep, target) => {
 var roleBuilder = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    this.updateBuildingStatus(creep);
+    updateBuildingStatus(creep);
     if (creep.memory.building) {
-      this.buildConstructionSite(creep, BUILD_PRIORITY);
+      buildConstructionSite(creep, BUILD_PRIORITY);
     } else {
-      this.obtainEnergy(creep);
-    }
-  },
-  /** @param {Creep} creep **/
-  updateBuildingStatus: function (creep) {
-    if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-      creep.memory.building = false;
-      creep.say("⛏");
-    }
-
-    if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-      creep.memory.building = true;
-      creep.say("🔨");
-    }
-  },
-  /** @param {Creep} creep **/
-  buildConstructionSite: function (creep, structureType = STRUCTURE) {
-    let filter;
-    if (structureType == "none") {
-      filter = (s) => true;
-    } else {
-      filter = (s) => s.structureType === structureType;
-    }
-    let target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {
-      filter: filter,
-    });
-    if (target) {
-      build(creep, target);
-    }
-  },
-  /** @param {Creep} creep **/
-  obtainEnergy: function (creep) {
-    if (creep.store.getFreeCapacity() > 0) {
-      pickupDroppedResources(creep) ||
-        assignCreepToObtainEnergyFromTombstone(creep) ||
-        assignCreepToObtainEnergyFromRuin(creep) ||
-        assignCreepToObtainEnergyFromStorage(creep) ||
-        (withdrawFromContainerOk() &&
-          assignCreepToObtainEnergyFromContainer(creep)) ||
-        (withdrawFromSpawnOk() && assignCreepToObtainEnergyFromSpawn(creep));
-    } else {
-      assignCreepToObtainEnergyFromSource(creep, BUILDER_SOURCE_INDEX);
+      obtainEnergy(creep);
     }
   },
 };
