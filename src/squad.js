0b18a796147eb6f2b2b4ddd9906dd867399d4866
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-22 20:42:51 -0700

    refactor squad

--- src/squad.js
+++ src/squad.js
@@ -7,53 +7,57 @@ const {
   rolePicker,
 } = require("./role");
 
-var squad = {
-  /**
-   * Assign jos to creeps based on their roles
-   */
-  assignJobs: function () {
-    for (var name in Game.creeps) {
-      var creep = Game.creeps[name];
-      switch (creep.memory.role) {
-        case "harvester":
-          roleHarvester.run(creep);
-          break;
-        case "upgrader":
-          roleUpgrader.run(creep);
-          break;
-        case "builder":
-          roleBuilder.run(creep);
-          break;
-        case "repairer":
-          roleRepairer.run(creep);
-          break;
-        case "miner":
-          roleMiner.run(creep);
-          break;
-        case "picker":
-          rolePicker.run(creep);
-          break;
-        default:
-          break;
-      }
+/**
+ * Assign jos to creeps based on their roles
+ */
+const assignJobs = () => {
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    switch (creep.memory.role) {
+      case "harvester":
+        roleHarvester.run(creep);
+        break;
+      case "upgrader":
+        roleUpgrader.run(creep);
+        break;
+      case "builder":
+        roleBuilder.run(creep);
+        break;
+      case "repairer":
+        roleRepairer.run(creep);
+        break;
+      case "miner":
+        roleMiner.run(creep);
+        break;
+      case "picker":
+        rolePicker.run(creep);
+        break;
+      default:
+        break;
     }
-  },
-  /**
-   * Get all creeps by role
-   * @param {string} creepRole
-   * @returns {Object<string, Creep>} a hash containing creeps given their role
-   **/
-  getTeam: function (creepRole) {
-    return _.filter(Game.creeps, (creep) => creep.memory.role == creepRole);
-  },
-  /**
-   * Get creep by name
-   * @param {string} creepName name of the creep to find
-   * @returns creep with the given name, undefined if not found
-   */
-  getCreep: (creepName) => {
-    return Game.creeps[creepName];
-  },
+  }
 };
 
-module.exports = squad;
+/**
+ * Get all creeps by role
+ * @param {string} creepRole
+ * @returns {Object<string, Creep>} a hash containing creeps given their role
+ **/
+const getTeam = (creepRole) => {
+  return _.filter(Game.creeps, (creep) => creep.memory.role == creepRole);
+};
+
+/**
+ * Get creep by name
+ * @param {string} creepName name of the creep to find
+ * @returns creep with the given name, undefined if not found
+ */
+const getCreep = (creepName) => {
+  return Game.creeps[creepName];
+};
+
+module.exports = {
+  assignJobs,
+  getTeam,
+  getCreep,
+};
