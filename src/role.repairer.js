ea6c617a8c7ce25aa0d5c63092262367b82eff86
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-23 02:54:21 -0700

    refactor repairer

--- src/role.repairer.js
+++ src/role.repairer.js
@@ -19,106 +19,110 @@ const {
   REPAIR_REGION_Y_UPPER,
 } = require("./dashboard");
 
+const updateRepairingStatus = (creep) => {
+  if (creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
+    creep.memory.repairing = false;
+    creep.say("⛏");
+  }
+
+  if (!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
+    creep.memory.repairing = true;
+    creep.say("🚧");
+  }
+};
+
+const repairConstruction = (creep) => {
+  let targets = findTargets(creep).sort((a, b) => a.hits - b.hits);
+  repairTargets(creep, targets);
+};
+
+const obtainEnergy = (creep) => {
+  if (creep.store.getFreeCapacity() > 0) {
+    pickupDroppedResources(creep) ||
+      assignCreepToObtainEnergyFromTombstone(creep) ||
+      assignCreepToObtainEnergyFromRuin(creep) ||
+      assignCreepToObtainEnergyFromContainer(creep) ||
+      assignCreepToObtainEnergyFromStorage(creep) ||
+      (withdrawFromContainerOk() &&
+        assignCreepToObtainEnergyFromContainer(creep)) ||
+      (withdrawFromSpawnOk() && assignCreepToObtainEnergyFromSpawn(creep));
+  } else {
+    assignCreepToObtainEnergyFromSource(creep, REPAIRER_SOURCE_INDEX);
+  }
+};
+
+const findTargets = (creep) => {
+  var decayedContainers = findDecayedStructure(creep, STRUCTURE_CONTAINER);
+  var decayedLinks = findDecayedStructure(creep, STRUCTURE_LINK);
+  var decayedStorage = findDecayedStructure(creep, STRUCTURE_STORAGE);
+  if (decayedStorage.length > 0) {
+    return decayedStorage;
+  } else if (decayedContainers.length > 0) {
+    return decayedContainers;
+  } else if (decayedLinks.length > 0) {
+    return decayedLinks;
+  } else {
+    var targets = creep.room.find(FIND_STRUCTURES, {
+      filter: getPrioritizedStructure,
+    });
+    return targets;
+  }
+};
+
+const repairTargets = (creep, targets) => {
+  if (targets.length > 0) {
+    if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(targets[0], {
+        visualizePathStyle: { stroke: "#ffffff" },
+      });
+    }
+  }
+};
+
+const findDecayedStructure = (creep, structureType) => {
+  let targets = creep.room.find(FIND_STRUCTURES, {
+    filter: (structure) =>
+      structure.structureType == structureType &&
+      structure.hits < structure.hitsMax,
+  });
+  return targets;
+};
+
+const getPrioritizedStructure = (structure) => {
+  let type = structure.structureType;
+  let needsRepair =
+    structure.hits < structure.hitsMax * REPAIR_HITS_THRESHOLD_RATIO;
+  let notMaxHits = structure.hits < structure.hitsMax;
+  switch (REPAIR_PRIORITY) {
+    case "walls":
+      return type == STRUCTURE_WALL && needsRepair;
+    case "roads":
+      return type == STRUCTURE_ROAD && notMaxHits;
+    case "ramparts":
+      return type == STRUCTURE_RAMPART && needsRepair;
+    case "infrastructure":
+      return (
+        type !== STRUCTURE_WALL && type !== STRUCTURE_RAMPART && notMaxHits
+      );
+    default:
+      return (
+        (needsRepair || notMaxHits) &&
+        structure.pos.x >= REPAIR_REGION_X_LOWER &&
+        structure.pos.x <= REPAIR_REGION_X_UPPER &&
+        structure.pos.y >= REPAIR_REGION_Y_LOWER &&
+        structure.pos.y <= REPAIR_REGION_Y_UPPER
+      );
+  }
+};
+
 let roleRepairer = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    this.updateRepairingStatus(creep);
+    updateRepairingStatus(creep);
     if (creep.memory.repairing) {
-      this.repairConstruction(creep);
+      repairConstruction(creep);
     } else {
-      this.obtainEnergy(creep);
-    }
-  },
-  updateRepairingStatus: function (creep) {
-    if (creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
-      creep.memory.repairing = false;
-      creep.say("⛏");
-    }
-
-    if (!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
-      creep.memory.repairing = true;
-      creep.say("🚧");
-    }
-  },
-  repairConstruction: function (creep) {
-    let targets = this.findTargets(creep).sort((a, b) => a.hits - b.hits);
-    this.repairTargets(creep, targets);
-  },
-  obtainEnergy: function (creep) {
-    if (creep.store.getFreeCapacity() > 0) {
-      pickupDroppedResources(creep) ||
-        assignCreepToObtainEnergyFromTombstone(creep) ||
-        assignCreepToObtainEnergyFromRuin(creep) ||
-        assignCreepToObtainEnergyFromContainer(creep) ||
-        assignCreepToObtainEnergyFromStorage(creep) ||
-        (withdrawFromContainerOk() &&
-          assignCreepToObtainEnergyFromContainer(creep)) ||
-        (withdrawFromSpawnOk() && assignCreepToObtainEnergyFromSpawn(creep));
-    } else {
-      assignCreepToObtainEnergyFromSource(creep, REPAIRER_SOURCE_INDEX);
-    }
-  },
-  findTargets: function (creep) {
-    var decayedContainers = this.findDecayedStructure(
-      creep,
-      STRUCTURE_CONTAINER
-    );
-    var decayedLinks = this.findDecayedStructure(creep, STRUCTURE_LINK);
-    var decayedStorage = this.findDecayedStructure(creep, STRUCTURE_STORAGE);
-    if (decayedStorage.length > 0) {
-      return decayedStorage;
-    } else if (decayedContainers.length > 0) {
-      return decayedContainers;
-    } else if (decayedLinks.length > 0) {
-      return decayedLinks;
-    } else {
-      var targets = creep.room.find(FIND_STRUCTURES, {
-        filter: this.getPrioritizedStructure,
-      });
-      return targets;
-    }
-  },
-  repairTargets: function (creep, targets) {
-    if (targets.length > 0) {
-      if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(targets[0], {
-          visualizePathStyle: { stroke: "#ffffff" },
-        });
-      }
-    }
-  },
-  findDecayedStructure: function (creep, structureType) {
-    let targets = creep.room.find(FIND_STRUCTURES, {
-      filter: (structure) =>
-        structure.structureType == structureType &&
-        structure.hits < structure.hitsMax,
-    });
-    return targets;
-  },
-  getPrioritizedStructure: function (structure) {
-    let type = structure.structureType;
-    let needsRepair =
-      structure.hits < structure.hitsMax * REPAIR_HITS_THRESHOLD_RATIO;
-    let notMaxHits = structure.hits < structure.hitsMax;
-    switch (REPAIR_PRIORITY) {
-      case "walls":
-        return type == STRUCTURE_WALL && needsRepair;
-      case "roads":
-        return type == STRUCTURE_ROAD && notMaxHits;
-      case "ramparts":
-        return type == STRUCTURE_RAMPART && needsRepair;
-      case "infrastructure":
-        return (
-          type !== STRUCTURE_WALL && type !== STRUCTURE_RAMPART && notMaxHits
-        );
-      default:
-        return (
-          (needsRepair || notMaxHits) &&
-          structure.pos.x >= REPAIR_REGION_X_LOWER &&
-          structure.pos.x <= REPAIR_REGION_X_UPPER &&
-          structure.pos.y >= REPAIR_REGION_Y_LOWER &&
-          structure.pos.y <= REPAIR_REGION_Y_UPPER
-        );
+      obtainEnergy(creep);
     }
   },
 };
