529dd4ba1a67002c52f7ad9a29938af290d3b164
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-24 20:13:59 -0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ const memoryManager = require("./util.memoryManager");
 const squad = require("./squad");
 const logger = require("./logger");
 const defense = require("./defense");
+const testScripts = require("./testScripts");
 const squadRecruiter = require("./squad.recruiter");
 
 module.exports.loop = function () {
   memoryManager.cleanNonExistingCreeps();
-  defense.activateTowers();
+  defense.activateTowers("W35N43");
+  defense.activateTowers("W36N43");
   squadRecruiter.run("W35N43");
   squadRecruiter.run("W36N43");
   squad.assignJobs();
-  logger.log();
+  logger.run();
+  testScripts.run();
 };
