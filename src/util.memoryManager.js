ac91f9b8d1a3f0ec10c85aaa1f14ed673b1b40ba
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-28 04:11:08 -0700

    refactor

--- src/util.memoryManager.js
+++ src/util.memoryManager.js
@@ -1,21 +1,23 @@
-var memoryManager = {
-  /**
-   * Clean memory of died creeps
-   */
-  cleanNonExistingCreeps: function () {
-    for (var name in Memory.creeps) {
-      if (!Game.creeps[name]) {
-        delete Memory.creeps[name];
-        console.log("Clearing non-existing creep memory:", name);
-      }
+/**
+ * Clean memory of died creeps
+ */
+const cleanNonExistingCreeps = () => {
+  for (var name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory:", name);
     }
-  },
-  /**
-   * Delete hostile creep record in memory
-   */
-  clearHostileMemory: () => {
-    delete Memory.hostiles;
-  },
+  }
 };
 
-module.exports = memoryManager;
+/**
+ * Delete hostile creep record in memory
+ */
+const clearHostileMemory = () => {
+  delete Memory.hostiles;
+};
+
+module.exports = {
+  cleanNonExistingCreeps,
+  clearHostileMemory,
+};
