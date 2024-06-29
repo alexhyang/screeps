fe7ad157ef6015c2c89602b0692e9d3d6a5928a2
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-29 02:37:40 -0700

    update bash alias

--- screep-aliases.sh
+++ screep-aliases.sh
@@ -1,3 +1,1 @@
-alias dashboard="cp src/dashboard.js dist/dashboard.js; echo 'synced dashboard'"
-alias build="rm build/*; cp src/* build/"
-alias dist="cp src/* dist/; echo 'copied to dist'"
+alias dist="rm dist/*; cp src/* dist/; echo 'copied to dist'"
