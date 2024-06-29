2ec4f1683ed97a49f7502bf4b6dc07e50c967672
Alex Yang <alex.hyang.dev@gmail.com>
2024-06-29 02:49:44 -0700

    prevent transferrer from moving into dangerous room

--- src/role.transferrer.js
+++ src/role.transferrer.js
@@ -1,12 +1,27 @@
 const { obtainResource, transferTo } = require("./role.creepManager");
 const { getStorage, getContainers } = require("./util.structureFinder");
 
-const obtainFromOriginRoom = (creep, roomName) => {
-  if (creep.room.name == roomName) {
+/**
+ * Obtain energy from storage in the specified room
+ * @param {Creep} creep
+ * @param {string} originRoomName
+ */
+const obtainFromOriginRoom = (creep, originRoomName) => {
+  if (creep.room.name == originRoomName) {
     obtainResource(creep, ["storage"]);
   } else {
-    let storage = getStorage(Game.rooms[roomName]);
+    let storage = getStorage(Game.rooms[originRoomName]);
     creep.moveTo(storage);
+  }
+};
+
+/**
+ * Force the transferrer to move down if it is going to W36N44 room
+ * @param {Creep} creep
+ */
+const avoidDangerZone = (creep) => {
+  if (creep.room.name == "W36N43" && creep.pos.y <= 3) {
+    creep.moveTo(DOWN);
   }
 };
 
@@ -16,10 +31,12 @@ module.exports = {
     if (creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0) {
       console.log(creep.name, "obtaining...");
       obtainFromOriginRoom(creep, originRoomName);
+      avoidDangerZone(creep);
     } else {
       console.log(creep.name, "transferring...");
       let container = getContainers(Game.rooms[dstRoomName])[0];
       let storage = getStorage(Game.rooms[dstRoomName]);
+      avoidDangerZone(creep);
       if (container.store.getFreeCapacity(RESOURCE_ENERGY) >= 400) {
         transferTo(creep, container);
       } else {
