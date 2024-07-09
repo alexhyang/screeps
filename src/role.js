688eafe39d55098c9e3176a3de981fcd56c58a2c
Alex Yang <alex.hyang.dev@gmail.com>
2024-07-08 22:16:44 -0700

    add notes about new role creation

--- src/role.js
+++ src/role.js
@@ -1,3 +1,13 @@
+// Methods to change when adding new roles:
+//   - roleNewRole.js (add new role module)
+//   - role.js (update role index)
+//   - jobManager.js (update assignJobs())
+//   - dashboard.js (add role config object)
+//   - recruiter.js (add recruitNewRole(), update recruitForRoom())
+
+//   - squad.js (update getTeamMaxSize())
+//   - logger.squad.js (update squadLogger())
+
 const roleHarvester = require("./role.harvester");
 const roleMiner = require("./role.miner");
 const roleUpgrader = require("./role.upgrader");
