3f08c21d67795aa8e016c6eab9ce8eadc3894edd
Alex Yang <alex.hyang.dev@gmail.com>
2024-07-08 21:35:25 -0700

    convert extractor to repairer when can't harvest from mineral

--- src/role.extractor.js
+++ src/role.extractor.js
@@ -3,6 +3,10 @@ const { getStorage, getExtractor } = require("./util.structureFinder");
 
 const harvestMineral = (creep) => {
   let mineral = creep.room.find(FIND_MINERALS)[0];
+  if (mineral.mineralAmount == 0) {
+    creep.memory = { role: "repairer" };
+    return;
+  }
   if (!creep.memory.resourceTypes && mineral) {
     creep.memory.resourceTypes = [];
     creep.memory.resourceTypes.push(mineral.mineralType);
@@ -10,6 +14,8 @@ const harvestMineral = (creep) => {
   let extractor = getExtractor(creep.room);
   if (extractor) {
     harvestFrom(creep, mineral);
+  } else {
+    creep.memory = { role: "repairer" };
   }
 };
 
