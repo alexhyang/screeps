f8a95f57af4de910679b1efcabc9026f562cee78
Alex Yang <alex.hyang.dev@gmail.com>
2024-08-19 17:11:42 -0700

    add shorthand for resource store

--- src/util.resourceManager.js
+++ src/util.resourceManager.js
@@ -2,6 +2,30 @@ const { getRoomConfig } = require("./configAPI");
 const { getContainers, getStorage } = require("./util.structureFinder");
 
 /**
+ * Shorthand for roomObj.store.getUsedCapacity()
+ * @param {RoomObject} roomObj
+ * @param {string} resourceType
+ * @returns {number} used capacity of specified resource type
+ */
+const getUsedCapacity = (roomObj, resourceType = RESOURCE_ENERGY) => {
+  if (roomObj.store) {
+    return roomObj.store.getUsedCapacity(resourceType);
+  }
+};
+
+/**
+ * Shorthand for roomObj.store.getFreeCapacity()
+ * @param {RoomObject} roomObj
+ * @param {string} resourceType
+ * @returns {number} free capacity of specified resource type
+ */
+const getFreeCapacity = (roomObj, resourceType = RESOURCE_ENERGY) => {
+  if (roomObj.store) {
+    return roomObj.store.getFreeCapacity(resourceType);
+  }
+};
+
+/**
  * Get the energy available in the room
  * @param {Room} room
  * @returns {number} the available energy in the given room
@@ -38,27 +62,30 @@ const withdrawFromSpawnOk = (room) => {
 };
 
 /**
+ * Determine if sources in the specified room are empty
+ * @param {Room} room
+ * @returns {boolean} true if all sources are empty, false otherwise
+ */
+const allSourcesAreEmpty = (room) => {
+  let sources = room.find(FIND_SOURCES);
+  return sources.filter((s) => s.energy > 0).length == 0;
+};
+
+/**
  * Determine if it's ok to withdraw energy from containers in the room
+ * @param {StructureContainer} container
  * @param {Room} room
  * @returns {boolean} true if it is okay to withdraw from container(s) in the
  *    given room, false otherwise
  **/
-const withdrawFromContainerOk = (room) => {
+const withdrawFromContainerOk = (container, room) => {
   const { CONTAINER_WITHDRAW_THRESHOLD } = getRoomConfig(room.name);
-  const { sourceIndex } = getRoomConfig(room.name).miner;
-  let minerSource = room.find(FIND_SOURCES)[sourceIndex];
-  let containers = getContainers(room);
 
-  for (let i in containers) {
-    if (
-      minerSource.energy == 0 ||
-      containers[i].store.getUsedCapacity(RESOURCE_ENERGY) >=
-        CONTAINER_WITHDRAW_THRESHOLD
-    ) {
-      return true;
-    }
+  if (allSourcesAreEmpty(room)) {
+    return true;
+  } else {
+    return getUsedCapacity(container) >= CONTAINER_WITHDRAW_THRESHOLD;
   }
-  return false;
 };
 
 /**
@@ -69,15 +96,12 @@ const withdrawFromContainerOk = (room) => {
  **/
 const withdrawFromStorageOk = (room) => {
   const { STORAGE_WITHDRAW_THRESHOLD } = getRoomConfig(room.name);
-  const { sourceIndex } = getRoomConfig(room.name).miner;
-  let minerSource = room.find(FIND_SOURCES)[sourceIndex];
   let storage = getStorage(room);
 
   if (
     storage &&
-    (minerSource.energy == 0 ||
-      storage.store.getUsedCapacity(RESOURCE_ENERGY) >=
-        STORAGE_WITHDRAW_THRESHOLD)
+    (allSourcesAreEmpty(room) ||
+      getUsedCapacity(storage) >= STORAGE_WITHDRAW_THRESHOLD)
   ) {
     return true;
   }
@@ -85,6 +109,8 @@ const withdrawFromStorageOk = (room) => {
 };
 
 module.exports = {
+  getUsedCapacity,
+  getFreeCapacity,
   getEnergyAvailable,
   getEnergyCapacityAvailable,
   withdrawFromContainerOk,
