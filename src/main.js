7d9e1bece96fd94e00b5585d56580ee448cf6391
Alex Yang <alex.hyang.dev@gmail.com>
2024-08-29 00:23:31 -0700

    add market operations

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ const defense = require("./defense");
 const testScripts = require("./testScripts");
 const squadRecruiter = require("./squad.recruiter");
 const structureSetup = require("./structure.setup");
+const market = require("./market");
 
 module.exports.loop = function () {
   memoryManager.run();
   logger.run();
+  market.dealTransactions();
+
   defense.activateTowers();
   structureSetup.run();
   squadRecruiter.run();
