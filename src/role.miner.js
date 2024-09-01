12052ae7f7c9eb7a41e73d02f94d6324f47be462
Alex Yang <alex.hyang.dev@gmail.com>
2024-09-01 00:41:51 -0700

    assign miners to each source in room

--- src/role.miner.js
+++ src/role.miner.js
@@ -1,11 +1,61 @@
-const { getRoomConfig } = require("./configAPI");
+const { getNearbyStructures } = require("./Creep");
 const { obtainResource, transferResource } = require("./CreepResource");
+const { getTeam } = require("./squad");
 const {
-  getContainers,
-  structureHasFreeCapacity,
-  getSpawns,
-  getExtensions,
-} = require("./util.structureFinder");
+  getFreeCapacity,
+  getCapacity,
+  storeHasSpace,
+} = require("./util.resourceManager");
+const { getSpawns, getExtensions } = require("./util.structureFinder");
+
+/**
+ * Set source index in creep memory
+ * @param {Creep} creep
+ * @param {number} index
+ */
+const setSrcIndex = (creep, index) => {
+  creep.memory.srcIndex = index;
+};
+
+/**
+ * Assign source index for newly recruited miner creep
+ * @param {Creep} creep
+ */
+const assignSrcIndex = (creep) => {
+  if (creep.memory.srcIndex == undefined) {
+    let sources = creep.room.find(FIND_SOURCES);
+    switch (sources.length) {
+      case 1:
+        setSrcIndex(creep, 0);
+        return;
+      case 2:
+        let miners = getTeam("miner", creep.room.name);
+        let index;
+        switch (miners.length) {
+          case 1:
+            console.log("miner team size", miners.length, "assigning", 0);
+            setSrcIndex(creep, 0);
+            return;
+          case 2:
+            console.log("miner team size", miners.length, "assigning", index);
+            index = miners[0].memory.srcIndex == 0 ? 1 : 0;
+            setSrcIndex(creep, index);
+            return;
+          default:
+            console.log("miner team size", miners.length, "assigning", index);
+            let dyingMiner = miners[0];
+            index =
+              dyingMiner.memory.srcIndex == undefined
+                ? 0
+                : dyingMiner.memory.srcIndex;
+            setSrcIndex(creep, index);
+            return;
+        }
+      default:
+        return;
+    }
+  }
+};
 
 /**
  * Find a target to deliver energy for the miner creep
@@ -15,25 +65,32 @@ const {
  */
 const findDeliveryTarget = (creep) => {
   if (creep) {
-    let containersNotFull = _.filter(getContainers(creep.room), (s) =>
-      structureHasFreeCapacity(s)
+    let nearbyLinks = getNearbyStructures(creep, STRUCTURE_LINK).filter(
+      (s) => getFreeCapacity(s) >= getCapacity(creep)
     );
-    if (containersNotFull.length > 0) {
-      return creep.pos.findClosestByRange(containersNotFull);
-    }
+    let nearbyContainers = getNearbyStructures(
+      creep,
+      STRUCTURE_CONTAINER
+    ).filter((s) => getFreeCapacity(s) >= getCapacity(creep));
 
-    let extensionsNotFull = _.filter(getExtensions(creep.room), (s) =>
-      structureHasFreeCapacity(s)
-    );
-    if (extensionsNotFull.length > 0) {
-      return creep.pos.findClosestByRange(extensionsNotFull);
-    }
+    if (nearbyLinks.length > 0) {
+      return nearbyLinks[0];
+    } else if (nearbyContainers.length > 0) {
+      return nearbyContainers[0];
+    } else {
+      let extensionsNotFull = _.filter(getExtensions(creep.room), (s) =>
+        storeHasSpace(s)
+      );
+      if (extensionsNotFull.length > 0) {
+        return creep.pos.findClosestByRange(extensionsNotFull);
+      }
 
-    let spawnsNotFull = _.filter(getSpawns(creep.room), (s) =>
-      structureHasFreeCapacity(s)
-    );
-    if (spawnsNotFull.length > 0) {
-      return creep.pos.findClosestByRange(spawnsNotFull);
+      let spawnsNotFull = _.filter(getSpawns(creep.room), (s) =>
+        storeHasSpace(s)
+      );
+      if (spawnsNotFull.length > 0) {
+        return creep.pos.findClosestByRange(spawnsNotFull);
+      }
     }
   }
 };
@@ -41,12 +98,14 @@ const findDeliveryTarget = (creep) => {
 module.exports = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    const { sourceOrigins, sourceIndex } = getRoomConfig(creep.room.name).miner;
-    if (creep.store.getFreeCapacity() > 0) {
-      obtainResource(creep, sourceOrigins, sourceIndex);
+    assignSrcIndex(creep);
+    if (getFreeCapacity(creep) > 0) {
+      obtainResource(creep, ["source"], creep.memory.srcIndex);
     } else {
       let target = findDeliveryTarget(creep);
-      transferResource(creep, target);
+      if (target) {
+        transferResource(creep, target);
+      }
     }
   },
 };
