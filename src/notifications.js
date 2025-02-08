c95cc966790ad43d46390b9745863c6c69e220eb
Alex Yang <alex.hyang.dev@gmail.com>
2025-02-08 01:13:27 -0800

    improve email notification

--- src/notifications.js
+++ src/notifications.js
@@ -1,21 +1,39 @@
 const { getMyRooms } = require("./configAPI");
 const { getTeam } = require("./squad");
-const {
-  getEnergyAvailable,
-  getEnergyCapacityAvailable,
-} = require("./util.resourceManager");
-const { getRoom } = require("./utils.game");
+const { getResourceMeta, getDefenseMeta } = require("./Room");
 
 module.exports = {
   run: function () {
-    getMyRooms().forEach((roomName) => {
-      let room = getRoom(roomName);
-      let energyAvailable = getEnergyAvailable(room);
-      let energyCapacityAvailable = getEnergyCapacityAvailable(room);
+    let msg;
+    const timeNow = new Date(Date.now());
+    const hrNow = timeNow.getHours();
+    const minNow = timeNow.getMinutes();
+    const secNow = timeNow.getSeconds();
+    // TODO: fix logic later, no creep is only checked at 12pm and 4pm
+    if (
+      (!Memory.lastSentHour || Memory.lastSentHour != hrNow) &&
+      (hrNow == 20 || hrNow == 4) &&
+      minNow == 0 &&
+      secNow <= 5
+    ) {
+      getMyRooms().forEach((roomName) => {
+        // UTC-->Pacific: 20:00 -> 12:00, 4:00 -> 20:00
+        msg =
+          roomName +
+          ": " +
+          getResourceMeta(roomName) +
+          ", " +
+          getDefenseMeta(roomName);
+        Game.notify(msg);
+        if (getTeam("all", roomName).length == 0) {
+          Game.notify(`No creep working in ${roomName}`, 30);
+        }
+      });
+      Memory.lastSentHour = hrNow;
+    }
 
-      if (Game.time % 1500 == 0) {
-        Game.notify(roomName, `${energyAvailable}/${energyCapacityAvailable}`);
-      }
+    getMyRooms().forEach((roomName) => {
+      // UTC-->Pacific: 20:00 -> 12:00, 4:00 -> 20:00
       if (getTeam("all", roomName).length == 0) {
         Game.notify(`No creep working in ${roomName}`, 30);
       }
